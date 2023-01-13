97efccaf368dda25b15100cf37c0e6d3f300155c
best-jinny <iwilldotherightthings@gmail.com>
2023-01-13 16:15:15 +0900

    #17 -  서비스 로직 테스트 코드 업데이트 및 DTO 재설계, 쿼리 메소드 작성
    나머지 로직 테스트 코드 작성, 연관된 DTO 재설계
    테스트가 필요로 하는 repository 쿼리 메소드도 작성

--- board-project/src/main/java/com/demo/boardproject/service/ArticleService.java
+++ board-project/src/main/java/com/demo/boardproject/service/ArticleService.java
@@ -2,14 +2,13 @@ package com.demo.boardproject.service;
 
 import com.demo.boardproject.domain.type.SearchType;
 import com.demo.boardproject.dto.ArticleDto;
-import com.demo.boardproject.dto.ArticleUpdateDto;
+import com.demo.boardproject.dto.ArticleWithCommentsDto;
 import com.demo.boardproject.repository.ArticleRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
 
 @RequiredArgsConstructor
 @Transactional
@@ -19,20 +18,19 @@ public class ArticleService {
     private final ArticleRepository articleRepository;
 
     @Transactional(readOnly = true)
-    public Page<ArticleDto> searchArticles(SearchType title, String search_keyword) {
+    public Page<ArticleDto> searchArticles(SearchType searchType, String search_keyword, Pageable pageable) {
         return Page.empty();
     }
 
     @Transactional(readOnly = true)
-    public ArticleDto searchArticle(long l) {
+    public ArticleWithCommentsDto getArticle(Long articleId) {
         return null;
     }
 
     public void saveArticle(ArticleDto dto) {
-
     }
 
-    public void updateArticle(long articleId, ArticleUpdateDto dto) {
+    public void updateArticle(ArticleDto dto) {
     }
 
     public void deleteArticle(long articleId) {
